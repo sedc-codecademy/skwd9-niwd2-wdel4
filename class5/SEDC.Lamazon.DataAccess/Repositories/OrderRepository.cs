7967dbdc898ef55ca44cee5f07a7bceeb2012be1
petreArsovski <email@example.comarso.pet@outlook.com>
2021-11-01 20:55:50 +0100

    class 5

---
+++ class5/SEDC.Lamazon.DataAccess/Repositories/OrderRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEDC.Lamazon.DataAccess.Interfaces;
+using SEDC.Lamazon.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEDC.Lamazon.DataAccess.Repositories
+{
+    public class OrderRepository : BaseRepository, IRepository<Order>
+    {
+        public OrderRepository(LamazonDbContext context) : base(context) { }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Order> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
