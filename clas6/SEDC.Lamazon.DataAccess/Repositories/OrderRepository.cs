b7d691bcc97ec2a67238509d3558cf4e8a4e6985
Manass2 <panemanass2@gmail.com>
2021-11-03 18:20:17 +0100

    class 6

---
+++ clas6/SEDC.Lamazon.DataAccess/Repositories/OrderRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEDC.Lamazon.DataAccess.Interfaces;
+using SEDC.Lamazon.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEDC.Lamazon.DataAccess.Repositories
+{
+    public class OrderRepository : BaseRepository, IRepository<Order>
+    {
+        public OrderRepository(LamazonDbContext context) : base(context) { }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Order> GetAll()
+        {
+            return _db.Orders
+                .Include(x => x.ProductOrders)
+                .ThenInclude(x => x.Product)
+                .Include(x => x.User);
+        }
+
+        public Order GetById(int id)
+        {
+            return _db.Orders
+                 .Include(x => x.ProductOrders)
+                 .ThenInclude(x => x.Product)
+                 .Include(x => x.User)
+                 .SingleOrDefault(x => x.Id == id);
+        }
+
+        public int Insert(Order entity)
+        {
+            _db.Orders.Add(entity);
+            return _db.SaveChanges();
+        }
+
+        public int Update(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ clas6/SEDC.Lamazon.Services/Helpers/DIModule.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SEDC.Lamazon.DataAccess;
+using SEDC.Lamazon.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Lamazon.Services.Helpers
+{
+    public static class DIModule
+    {
+        public static IServiceCollection RegisterModule(IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<LamazonDbContext>
+                (options => options.UseSqlServer(connectionString));
+
+            services
+            .AddIdentity<User, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = false;
+                options.Password.RequireNonAlphanumeric = false;
+            })
+            .AddRoleManager<RoleManager<IdentityRole>>()
+            .AddEntityFrameworkStores<LamazonDbContext>()
+            .AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
